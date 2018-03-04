27f8eb7962cb3f222d851316959e94b64bf31bd9
Haihao Yan <hyan36@gmal.com>
2018-03-04 18:05:19 +1100

    change api to use adapter

--- src/test/java/com/oocode/BetPlacerTests.java
+++ src/test/java/com/oocode/BetPlacerTests.java
@@ -4,89 +4,91 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.teamoptimization.Quote;
-
-import org.junit.*;
-
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when; 
 import java.math.BigDecimal;
 
 public class BetPlacerTests {
 
-	protected Boolean accepted;
+	protected Boolean accepted = false;
 
-	protected Boolean agreed;
+	protected Boolean agreed = false;
 	
-	protected String acceptedId;
+	protected String acceptedId = null;	
+
+	protected int expensiveOdds = 100;
 
 	protected BetPlacer betPlacer;
 	
-	protected int expensiveOdds;
+	
+	protected ISlugRacingOddsApiAdapter slugRacingOddsAdapter = mock(ISlugRacingOddsApiAdapter.class);
+	
+	protected ISlugSwapApiAdapter slugSwapApiAdapter = mock(ISlugSwapApiAdapter.class);
 
 	@Before
 	public void initialize() {
-		accepted = false;
-		agreed = false;
-		acceptedId = null;
-		expensiveOdds = 100;
-		betPlacer = new BetPlacer() {
-			@Override
-			public String getP2PQuote(int slugId, String raceName, BigDecimal targetOdds) {
-				return acceptedId;
-			}
-			@Override
-			public void agreeExpensiveOdds(Quote b) {
-				agreed = true;
-			}
-			@Override
-			public Quote getExpensiveOdds(int slugId, String raceName) {
-				return new Quote(new BigDecimal(expensiveOdds), raceName);
-			}
-			@Override
-			public void acceptCheapOdds(String p2p) {
-				accepted = true;
-			}
-		};
+		when(slugRacingOddsAdapter.getExpensiveOdds(002, "something")).thenReturn(new Quote(new BigDecimal(100), "something"));		
+		betPlacer = new BetPlacer(this.slugRacingOddsAdapter, this.slugSwapApiAdapter);
 	}
 
 	@Test
 	public void usesCheaperProviderIfOddsTheSame() throws Exception {
 		acceptedId = "somethingrandomid";
+		when(slugSwapApiAdapter.getP2PQuote(002, "something", new BigDecimal(100))).thenReturn(acceptedId);
+		
 		betPlacer.placeBet(002, "something", new BigDecimal(100));
-		Assert.assertEquals(false, agreed);
-		Assert.assertEquals(true, accepted);
+		verify(slugSwapApiAdapter).acceptCheapOdds(acceptedId);
+		verify(slugRacingOddsAdapter, never()).agreeExpensiveOdds(new Quote(new BigDecimal(expensiveOdds), "something"));
 	}
 
 	@Test
 	public void usesCheaperProviderIfOddsBetter() throws Exception {
 		acceptedId = "somethingrandomid";
+		when(slugSwapApiAdapter.getP2PQuote(002, "something", new BigDecimal(102))).thenReturn(acceptedId);
 		betPlacer.placeBet(002, "something", new BigDecimal(102));
-		Assert.assertEquals(false, agreed);
-		Assert.assertEquals(true, accepted);
+		verify(slugSwapApiAdapter).acceptCheapOdds(acceptedId);
+		verify(slugRacingOddsAdapter, never()).agreeExpensiveOdds(new Quote(new BigDecimal(expensiveOdds), "something"));
 	}
 
 	@Test
 	public void usesExpensiveProviderIfOddsBetter() throws Exception {
-		acceptedId = null;
+		acceptedId = "somethingrandomid";
+		when(slugSwapApiAdapter.getP2PQuote(002, "something", new BigDecimal(100))).thenReturn(acceptedId);	
+		
 		expensiveOdds = 102;
+		Quote quote = new Quote(new BigDecimal(expensiveOdds), "something");
+		when(slugRacingOddsAdapter.getExpensiveOdds(002, "something")).thenReturn(quote);
+		
 		betPlacer.placeBet(002, "something", new BigDecimal(100));
-		Assert.assertEquals(true, agreed);
-		Assert.assertEquals(false, accepted);
+		verify(slugSwapApiAdapter, never()).acceptCheapOdds(acceptedId);
+		verify(slugRacingOddsAdapter).agreeExpensiveOdds(quote);
 	}
 
 	@Test
 	public void placesExpensiveBetIfTargetOddsNotMetOnCheapBet() throws Exception {
 		acceptedId = null;
+		when(slugSwapApiAdapter.getP2PQuote(002, "something", new BigDecimal(100))).thenReturn(acceptedId);	
+		
 		expensiveOdds = 102;
+		Quote quote = new Quote(new BigDecimal(expensiveOdds), "something");
+		when(slugRacingOddsAdapter.getExpensiveOdds(002, "something")).thenReturn(quote);
+		
 		betPlacer.placeBet(002, "something", new BigDecimal(100));
-		Assert.assertEquals(true, agreed);
-		Assert.assertEquals(false, accepted);
+		verify(slugSwapApiAdapter, never()).acceptCheapOdds(acceptedId);
+		verify(slugRacingOddsAdapter).agreeExpensiveOdds(quote);
 	}
 
 	@Test
 	public void placesNoBetIfTargetOddsNotMetOnEither() throws Exception {
 		acceptedId = null;
+		when(slugSwapApiAdapter.getP2PQuote(002, "something", new BigDecimal(102))).thenReturn(acceptedId);	
+		
 		expensiveOdds = 100;
+		Quote quote = new Quote(new BigDecimal(expensiveOdds), "something");
+		when(slugRacingOddsAdapter.getExpensiveOdds(002, "something")).thenReturn(quote);
+		
 		betPlacer.placeBet(002, "something", new BigDecimal(102));
-		Assert.assertEquals(false, agreed);
-		Assert.assertEquals(false, accepted);
+		verify(slugSwapApiAdapter, never()).acceptCheapOdds(acceptedId);
+		verify(slugRacingOddsAdapter, never()).agreeExpensiveOdds(quote);
 	}
 }
